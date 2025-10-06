67aa544270f5911ba5360ce15227a29fb10fc037
Stefan Nožinić <stefan@lugons.org>
2025-10-07 01:11:53 +0200

    Add DebugProvider and EqualsOverride classes for debugging support

---
+++ neptune/providers/debug.py
@@ -0,0 +1,26 @@
+
+import  neptune.base_provider
+from neptune.lang import Identifier 
+
+
+class EqualsOverride:
+
+    def __init__(self, existing):
+        self.existing = existing
+    def evaluate(self, context, left, right):
+        name = left[0].name
+        print("Assigning to %s value %s" % (name, right))
+        self.existing.evaluate(context, left, right)
+
+class DebugProvider(neptune.base_provider.LibraryProvider):
+    def provide(self, ctx, name, args):
+        if name == "debug":
+            op, prio = ctx["="]
+            ctx["="] = (EqualsOverride(op), prio)
+            self.identifiers = ctx.identifiers.copy()  
+            return True
+        return False
+    def finish(self, ctx):
+        for k,v in ctx.identifiers.items():
+            if k not in self.identifiers:
+                print("%s = %s" % (k, v))
